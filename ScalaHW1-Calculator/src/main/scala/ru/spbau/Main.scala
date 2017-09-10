49f3a4bed869f8be6e3422891e462db3da26ff4f
tbelova <yukikomodo@gmail.com>
2017-09-10 12:37:09 +0300

    Add console interface.

--- ScalaHW1-Calculator/src/main/scala/ru/spbau/Main.scala
+++ ScalaHW1-Calculator/src/main/scala/ru/spbau/Main.scala
@@ -3,8 +3,11 @@ package ru.spbau
 object Main {
 
   def main(args: Array[String]) = {
-    val expr = "(2 + (2 * 3 + (1 + 2 + 3) * 2) / 2 + 1) / 3 / 4 - 1 - 2 - 3 * 2 - 12 + 8 + 5"
-    System.out.println(Calculator.evaluate(expr))
+    try {
+      System.out.println(Calculator.evaluate(args(0)))
+    } catch {
+      case _: ParseError => System.out.println("Parse Error")
+    }
   }
 
 }
